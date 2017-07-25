99acfa208283893fcf9726c22460a6c50ccc1e0f
Ibrahim Jarif <jarifibrahim@gmail.com>
2017-07-25 15:18:49 +0530

    Hound CI Test
    DO NOT merge. This is a test commit.

--- oshc/main/views.py
+++ oshc/main/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+import re;
+
+
 
 def home(request):
     return render(request, 'index.html')
 
 def request_session(request):
     return render(request, 'session.html')
+
+
+
+
+def RANDOM_VIEW(request):
+    str = "This is a test string. The length of this string is more than 80 chars. Hound CI should report this line as it violated the PEP8 convention. Some more random words to increase the string length".
+
+    
+    
+    
+    // Whitespce
